576399ec47a58f6108f12fbaa42f06893c5efda7
moviefan322 <philman202@yahoo.com>
2023-07-08 23:43:29 -0400

    adds validators to User model

--- app/models/User.py
+++ app/models/User.py
@@ -1,5 +1,6 @@
 from app.db import Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import validates
 
 class User(Base):
     __tablename__ = 'users'
@@ -7,3 +8,17 @@ class User(Base):
     username = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False, unique=True)
     password = Column(String(100), nullable=False)
+
+    @validates('email')
+    def validate_email(self, key, email):
+        # make sure email address contains @ character
+        assert '@' in email
+
+        return email
+    
+    @validates('password')
+    def validate_password(self, key, password):
+        # make sure password is at least 6 characters long
+        assert len(password) > 6
+
+        return password
